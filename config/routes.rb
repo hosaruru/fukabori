42fa2d336d1eb01c15a64ea29962587ed635097e
EC2 Default User <ec2-user@ip-172-31-25-210.ec2.internal>
2023-01-08 06:27:50 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'fukabori_contents#index'
+  devise_for :users
   
   resources :fukabori_contents, only: [:index, :new, :create, :show, :edit]
   patch 'fukabori_contents/:id' => 'fukabori_contents#update', as: 'update_fukabori_content'
@@ -11,13 +12,8 @@ Rails.application.routes.draw do
   resources :fukabori_texts, only: [:new, :create, :show, :edit]
   patch 'fukabori_texts/:id' => 'fukabori_texts#update', as: 'update_fukabori_text'
   delete 'fukabori_texts/:id' => 'fukabori_texts#destroy', as: 'destroy_fukabori_text'
-
-  resources :posts, only: [:index, :new, :create, :show] 
-  get 'posts/:id/edit' => 'posts#edit', as: 'edit_post'
-  delete 'posts/:id' => 'posts#destroy', as: 'destroy_post'
-  patch 'posts/:id' => 'posts#update', as: 'update_post'
   
-  resources :points, only: [:new, :show, :create]
+  resources :points, only: [:index, :new, :show, :create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
